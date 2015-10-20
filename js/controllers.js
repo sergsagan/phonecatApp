2e0e9df1ed8df423fdde10725eb5905debf3a070
sergey <sergey.martynuk@gmail.com>
2015-10-20 12:59:09 +0300

    ng-src angular

--- js/controllers.js
+++ js/controllers.js
@@ -5,3 +5,15 @@
 'use strict';
 
 /* Controllers */
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl',['$scope','$http', function($scope, $http) {
+
+	$http.get('phones/phones.json').success(function(data) {
+		$scope.phones = data;
+	});
+
+}]);
+//status, headers, config
+
