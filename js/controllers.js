5e90037beb7978cc19c050789c2a9affbb41443c
sergey <sergey.martynuk@gmail.com>
2015-10-21 12:01:30 +0300

    location&route

--- js/controllers.js
+++ js/controllers.js
@@ -6,14 +6,53 @@
 
 /* Controllers */
 
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatApp = angular.module('phonecatApp', ['ngRoute']);
 
-phonecatApp.controller('PhoneListCtrl',['$scope','$http', function($scope, $http) {
+phonecatApp.config(['$routeProvider', '$locationProvider', function($routeProvide, $locationProvider){
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+	$routeProvide
+		.when('/', {
+			templateUrl: 'template/home.html',
+			controller: 'PhoneListCtrl'
+		})
+		.when('/about', {
+			templateUrl: 'template/about.html',
+			controller: 'AboutCtrl'
+		})
+		.when('/contact', {
+			templateUrl: 'template/contact.html',
+			controller: 'ContactCtrl'
+		})
+		.when('/phones/:phoneId', {
+			templateUrl: 'template/phone-detail.html',
+			controller: 'PhoneDetailCtrl'
+		})
+		.otherwise ({
+			redirectTo: '/'
+		});
+}]);
 
-	$http.get('phones/phones.json').success(function(data) {
+phonecatApp.controller('PhoneListCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+
+	$http.get('phones/phones.json').success(function(data, status, headers, config) {
 		$scope.phones = data;
 	});
 
 }]);
-//status, headers, config
 
+//About Controller
+phonecatApp.controller('AboutCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+
+}]);
+
+//Contact Controller
+phonecatApp.controller('ContactCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+
+}]);
+//Phone-detail Controller
+phonecatApp.controller('PhoneDetailCtrl',['$scope','$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+	$scope.phoneId = $routeParams.phoneId;
+}]);
